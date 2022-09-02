d216885998ffb0523c75fb486ba4f63893a2de42
lan <le.hoang.lan@actindi.net>
2022-09-02 16:42:21 +0900

    bug

--- js/content-script.js
+++ js/content-script.js
@@ -1,5 +1,4 @@
 let selectionText = "";
-
 const bodyDOM = document.querySelector("body");
 
 function getSelectedTextNode() {
@@ -21,12 +20,11 @@ function getRangeSectionText() {
 }
 
 function renderbuttonTranslator(selectionTextRange, selectionText) {
-  const buttonWrapper = document.createElement("div");
+  const buttonWrapper = document.createElement("button");
   buttonWrapper.id = "kanji-to-furigana";
   const buttonIcon = document.createElement("div");
   buttonIcon.classList.add("kanji-translator-icon");
-  buttonIcon.innerHTML =
-    '<img src="chrome-extension://gckkkddkkdpaopiojcknljfbbnilooen/kanjitofurigana.svg"/>';
+  buttonIcon.innerHTML = `<svg width="20px" height="20px" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg"><g fill="#fff" fill-rule="evenodd" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" transform="translate(2 2)"><path d="m16.5 8.5v-6c0-1.1045695-.8954305-2-2-2h-6c-1.1045695 0-2 .8954305-2 2v6c0 1.1045695.8954305 2 2 2h6c1.1045695 0 2-.8954305 2-2z"/><path d="m4.5 6.50344846h-2.00001427c-1.1045695 0-2 .8954305-2 2v5.99943324c0 1.1045695.8954305 2 2 2h.00345627l6.00001428-.0103718c1.10321833-.0019065 1.99654372-.8967771 1.99654372-1.999997v-1.9925129"/><g transform="translate(2.502 9.5)"><path d="m2.998 1.003h-3"/><path d="m4.49841597 2.5c-.33333333.33333333-.66666667.66666667-1 1s-1.16666667.83333333-2.5 1.5"/><path d="m.99841597 1.00316806c.33333333 1.16613866.83333333 1.99894398 1.5 2.49841597s1.5.99894398 2.5 1.49841597"/></g><g transform="translate(8.5 2.5)"><path d="m3 0-3 6"/><path d="m3 0 3 6"/><path d="m3 2v4" transform="matrix(0 1 -1 0 7 1)"/></g></g></svg>`
   buttonWrapper.appendChild(buttonIcon);
   const top = selectionTextRange.top + selectionTextRange.height + 6 + "px";
   const left =
@@ -40,6 +38,23 @@ function renderbuttonTranslator(selectionTextRange, selectionText) {
   buttonWrapper.style.top = top;
   buttonWrapper.style.left = left;
   bodyDOM.appendChild(buttonWrapper);
+  if(buttonWrapper){
+    console.log('co button')
+    buttonWrapper.addEventListener("click", async () => {
+      console.log('okkkk')
+      if (selectionText.length > 0) {
+        fetch(
+          `http://localhost:3000/api/v1/search/${selectionText}`
+        ).then((res)=> {
+          renderbuttonResultTranslator(
+            selectionTextRange,
+            selectionText,
+            res
+          );
+        })
+      }
+    });
+  }
 }
 
 function renderbuttonResultTranslator(
@@ -47,12 +62,23 @@ function renderbuttonResultTranslator(
   selectionText,
   selectionTextTranslated
 ) {
+  console.log('ok')
   const buttonWrapper = document.createElement("div");
-  buttonWrapper.id = "translator-result-ext-rhpteam";
+  buttonWrapper.id = "kanji-to-furigana";
   const buttonContainer = document.createElement("div");
   buttonContainer.classList.add("translator-result-ext-container");
-  buttonContainer.innerHTML = chrome.runtime.getURL("popup.html");
-  buttonWrapper.appendChild(buttonContainer);
+  buttonContainer.innerHTML = `
+    <label>
+      Input:
+      <span>${selectionText}</span>
+    </label>
+    <br>
+    <label>
+      Output:
+      <span>${selectionTextTranslated}</span>
+    </label>
+  `; 
+   buttonWrapper.appendChild(buttonContainer);
   const top = selectionTextRange.top - selectionTextRange.height - 6 + "px";
   const left =
     selectionTextRange.left +
@@ -69,7 +95,7 @@ function renderbuttonResultTranslator(
 
 bodyDOM.addEventListener("mouseup", () => {
   const buttonResult = document.querySelector(
-    "div#translator-result-ext-rhpteam"
+    "button#kanji-to-furigana"
   );
   if (buttonResult) buttonResult.remove();
 
@@ -78,9 +104,5 @@ bodyDOM.addEventListener("mouseup", () => {
   if (selectionText.length > 0) {
     const selectionTextRange = getRangeSectionText();
     renderbuttonTranslator(selectionTextRange, selectionText);
-    setTimeout(() => {
-      const buttonWrapper = document.querySelector("div#kanji-to-furigana");
-      if (buttonWrapper) buttonWrapper.remove();
-    }, 3000);
   }
 });
