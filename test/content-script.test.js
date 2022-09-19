17bb3d3953b77b1124297ab74a87fb41ec6d85c0
lan <le.hoang.lan@actindi.net>
2022-09-19 21:37:06 +0900

    テスト完成

--- test/content-script.test.js
+++ test/content-script.test.js
@@ -1,9 +1,10 @@
 const puppeteer = require("puppeteer");
 const EXTENSION_PATH = "./";
-
-describe("テキストを選択するとき", () => {
-  test("アイコンが表示される", async () => {
-    const browser = await puppeteer.launch({
+describe("コンテントスクリプト", () => {
+  let browser;
+  let page;
+  beforeEach(async () => {
+    browser = await puppeteer.launch({
       headless: false,
       defaultViewport: null,
       args: [
@@ -12,20 +13,89 @@ describe("テキストを選択するとき", () => {
         `--window-size=800,600`,
       ],
     });
-    const page = await browser.newPage();
-    await page.goto("https://shinagawa.keizai.biz/headline/3170/");
+    page = await browser.newPage();
+    await page.goto(
+      "https://imikaisetu.goldencelebration168.com/archives/2061"
+    );
     await page.setViewport({ width: 1280, height: 800 });
-
-    const linkHandlers = await page.$x("//h1[contains(text(), '目的')]");
+    const linkHandlers = await page.$x("//em[contains(text(), '関連')]");
     await linkHandlers[0].click({ clickCount: 2 });
-    await page.waitForFunction(
-      'document.querySelector("body").getElementsByClassName("translator-ext-icon")[0] != undefined'
-    );
-    const button = await page.$("path");
-    await button.click()
-    await page.waitForFunction(
-      'document.querySelector("body").getElementsByClassName("translator-result-ext-container")[0] != undefined'
-    );
-    await browser.close();
+  });
+  describe("テキストを選択するとき", () => {
+    test("アイコンが表示される", async () => {
+      await page.waitForFunction(
+        'document.querySelector("body").getElementsByClassName("translator-ext-icon")[0] != undefined'
+      );
+      await browser.close();
+    });
+  });
+  describe("アイコンをクリックするとき", () => {
+    beforeEach(async () => {
+      const button = await page.$("path");
+      await button.click();
+    });
+    test("ベトナム語で言葉のページが表示される", async () => {
+      await page.waitForTimeout(2000);
+      await page.waitForFunction(
+        'document.querySelector("body").getElementsByClassName("translator-result-ext-container")[0] != undefined'
+      );
+      await page.waitForFunction(
+        "document.querySelector('#dic_bubble_synonyms > div > div > div.vocabulary_searched_pl').innerHTML === '関連'"
+      );
+      await page.waitForFunction(
+        'document.querySelector("#vocabulary_mean_group > div").innerHTML === "  sự liên quan; sự liên hệ; sự quan hệ; liên quan; liên hệ; quan hệ"'
+      );
+      await browser.close();
+    });
+    test("英語で言葉のページが表示される", async () => {
+      await page.waitForTimeout(2000);
+      const englishTab = await page.$("#tab_english");
+      englishTab.click();
+      await page.waitForFunction(
+        'document.querySelector("body").getElementsByClassName("translator-result-ext-container")[0] != undefined'
+      );
+      await page.waitForFunction(
+        "document.querySelector('#dic_bubble_synonyms > div > div > div.vocabulary_searched_pl').innerHTML === '関連'"
+      );
+      await page.waitForFunction(
+        'document.querySelector("#vocabulary_mean_group > div").innerHTML === " a term in a proposition that is related to the referent of the proposition"'
+      );
+      await browser.close();
+    });
+  });
+  describe("漢字タブをクリックするとき", () => {
+    beforeEach(async () => {
+      const button = await page.$("path");
+      await button.click();
+    });
+    test("ベトナム語で漢字コンテントが表示される", async () => {
+      await page.waitForTimeout(2000);
+      const kanjiTab = await page.$("#tab_kanji");
+      await page.waitForFunction(
+        "!document.querySelector('#tab_kanji').className.includes('tab_active')"
+      );
+      await kanjiTab.click();
+      await page.waitForFunction(
+        "document.querySelector('.kanji_chinese_meaning').innerHTML === 'QUAN'"
+      );
+      await browser.close();
+    });
+    test("英語で漢字コンテントが表示される", async () => {
+      await page.waitForTimeout(2000);
+      const englishTab = await page.$("#tab_english");
+      englishTab.click();
+      await page.waitForTimeout(2000);
+      const kanjiTab = await page.$("#tab_kanji");
+      await page.waitForFunction(
+        "!document.querySelector('#tab_kanji').className.includes('tab_active')"
+      );
+      await kanjiTab.click();
+      await page.waitForFunction(
+        "document.querySelector('.kanji_chinese_meaning').innerHTML === 'CONNECTION'"
+      );
+      await browser.close();
+    });
   });
 });
+ 
+
